849892c9b5900ec9792dd7bf379dec0aeae34b48
oleg9 <olega_djan@mail.ru>
2025-07-05 10:47:15 +0300

    create config.py: for load settings from environment

---
+++ app/config.py
@@ -0,0 +1,24 @@
+from pathlib import Path
+from pydantic_settings import BaseSettings
+
+
+class Settings(BaseSettings):
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str
+    POSTGRES_DB: str
+    POSTGRES_HOST: str = "db"
+    POSTGRES_PORT: int = 5432
+
+    @property
+    def POSTGRES_URL(self):
+        return (
+            f"postgresql+psycopg2://{self.POSTGRES_USER}:{self.POSTGRES_PASSWORD}@"
+            f"{self.POSTGRES_HOST}:{self.POSTGRES_PORT}/{self.POSTGRES_DB}"
+        )
+
+    class Config:
+        env_file = Path(__file__).parent.parent.parent / ".env"
+        env_file_encoding = "utf-8"
+
+
+settings = Settings()
