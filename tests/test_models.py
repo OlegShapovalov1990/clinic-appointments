85d5a3d3cc1ba319345a80a457cade9b3d8e1be2
oleg9 <olega_djan@mail.ru>
2025-07-05 14:08:28 +0300

    create test_models.py: add checks the correctness of the calculation of the end time of the reception

---
+++ tests/test_models.py
@@ -0,0 +1,11 @@
+from datetime import datetime, timedelta
+from app.models import Appointment
+
+
+def test_calculate_end_time():
+    start_time = datetime(2023, 1, 1, 10, 0)
+    end_time = Appointment.calculate_end_time(start_time)
+    assert end_time == datetime(2023, 1, 1, 10, 30)
+
+    end_time_custom = Appointment.calculate_end_time(start_time, duration=45)
+    assert end_time_custom == datetime(2023, 1, 1, 10, 45)
